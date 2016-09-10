e956455f66ab438de3356e321a18b8063d816b88
Justin Lange <well1912@gmail.com>
2016-09-09 23:20:16 -0700

    Implement alpha-rename-rec

--- lambda.scm
+++ lambda.scm
@@ -48,5 +48,26 @@
 (define (build-var n)
   (string->symbol (string-append "var" (number->string n))))
 
+(define (assq-default x lst)
+  (let ((search (assq x lst)))
+    (if search (cdr search) x)))
+
 (define (alpha-rename-rec in env n)
-  (values in env n))
+  (cond
+   ((application? in)
+    (let*-values (((op-body op-env op-n)
+                   (alpha-rename-rec (car in) env n))
+                  ((arg-body arg-env arg-n)
+                   (alpha-rename-rec (car (cdr in)) env op-n)))
+      (values (list op-body arg-body) env op-n)))
+   ((abstraction? in)
+    (let ((var (build-var n)))
+      (let-values (((renamed-body new-env new-n)
+                    (alpha-rename-rec (abstraction-body in)
+                                      (cons (cons (abstraction-var in) var)
+                                            env)
+                                      (+ n 1))))
+        (values (list 'lambda (list var) renamed-body)
+                env
+                new-n))))
+   (else (values (assq-default in env) env n))))
