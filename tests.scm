ee714c0bccc684881d18f04ab4e133760d884b9b
Justin Lange <well1912@gmail.com>
2016-09-09 21:22:12 -0700

    Implement basic application test

--- tests.scm
+++ tests.scm
@@ -9,4 +9,7 @@
   (suite constants
     (test 'x (calculate-lambda 'x))
     (test 'y (calculate-lambda 'y))
-    (test 0 (calculate-lambda 0))))
+    (test 0 (calculate-lambda 0)))
+
+  (suite applications
+    (test '0 (calculate-lambda '((lambda (x) x) 0)))))
