6c162ce465c0ca1cf0e34dfdf26c45313c11aba8
Justin Lange <well1912@gmail.com>
2016-09-09 21:28:42 -0700

    Implement basic application?

--- lambda2.scm
+++ lambda2.scm
@@ -1,12 +1,15 @@
 (define-library (lambda-calculator main)
   (import (scheme base))
-  (export abstraction? calculate-lambda)
+  (export abstraction? application? calculate-lambda)
   (begin
     (define (abstraction? x)
       (and (list? x)
            (eq? (car x) 'lambda)
            (= (length x) 3)
            (list? (car (cdr x)))))
+
+    (define (application? x)
+      (pair? x))
 
     (define (calculate-lambda x)
       x)))
